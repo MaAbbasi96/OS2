5089ea9c5d386a21137418b01bc676899208c396
Mahdi Abbasi <ma.abbasi.9675@gmail.com>
2017-11-10 18:35:24 +0330

    Reading directroies added successfull... :D

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,17 @@
-#include <iostream>
+#include "functions.h"
+
+void read_directory(const std::string& dirName, std::vector<std::string>& directories){
+    DIR* dirPointer = opendir(dirName.c_str());
+    struct dirent* dp;
+    while((dp = readdir(dirPointer)) != NULL)
+        if(dp->d_name != "." && dp->d_name != "..")
+            directories.push_back(dp->d_name);
+    closedir(dirPointer);
+}
+
+int is_regular_file(const char* path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISREG(path_stat.st_mode);
+}
--- functions.h
+++ functions.h
@@ -1,6 +1,14 @@
 #ifndef FUNCTIOS_H
 #define FUNCTIOS_H
 
+#include <sys/types.h>
+#include <dirent.h>
+#include <string>
+#include <vector>
+#include <sys/stat.h>
+#include <unistd.h>
 
+void read_directory(const std::string& dirName, std::vector<std::string>& directories);
+int is_regular_file(const char* path);
 
 #endif
