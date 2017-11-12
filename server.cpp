1932718bab37776c47ffb399d52adac150c6583e
Mahdi Abbasi <ma.abbasi.9675@gmail.com>
2017-11-12 22:55:04 +0330

    forking processes and read/write using pipe added

--- server.cpp
+++ server.cpp
@@ -3,31 +3,72 @@
 #include <string>
 #include <vector>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <errno.h>
 #include <unistd.h>
 #include <fstream>
+#include <sstream>
 #include "functions.h"
+
+#define BUFSIZE 2048
+#define WRITE_FD 1
+#define READ_FD 0
 
 using namespace std;
 
-void calculate_fine(int tag, int& totalFine,const string& dirName){
+string int_to_str(int x){
+    stringstream res;
+    res << x;
+    return res.str();
+}
+
+void pipe_write(int fd, string str){
+    write(fd, str.c_str(), str.length());
+}
+
+string pipe_read(int fd){
+    char buf[BUFSIZE];
+    int length;
+    length = read(fd, buf, BUFSIZE);
+    buf[length] = '\0';
+    return string(buf);
+}
+
+int sum_of_fine_in_file(string fileName){ //TODO: reading from file 
+    return 10;
+}
+
+void calculate_fine(int tag, int parent_write_fd, string dirName){
+    int totalFine = 0;
     vector<string> directory;
+    string current_address;
     functions::read_directory(dirName, directory);
     int pfds[directory.size()][2];
+
+    //forking
     for(int i = 0; i < directory.size(); i++){
+        pipe(pfds[i]);
+        current_address = dirName + '/' + directory[i];
         if(!fork()){
-            //child process
-        }
-        else{
-            //parent process
+            if(functions::is_regular_file(current_address.c_str()) == 1)
+                pipe_write(pfds[i][WRITE_FD], int_to_str(sum_of_fine_in_file(current_address)));
+            else
+                calculate_fine(tag, pfds[i][WRITE_FD], current_address);
+            exit(0);
         }
     }
+
+    //terminating processes
+    for(int i = 0; i < directory.size(); i++){
+        totalFine += atoi(pipe_read(pfds[i][READ_FD]).c_str());
+        wait(NULL);
+    }
+    pipe_write(parent_write_fd, int_to_str(totalFine));
 }
 
 int main(){
-    int totalFine = 0;
     string startingDir = "./dir";
-    calculate_fine(12345, totalFine, startingDir);
-    cout << totalFine << endl;
+    calculate_fine(12345, STDOUT_FILENO, startingDir);
     return 0;
 }
